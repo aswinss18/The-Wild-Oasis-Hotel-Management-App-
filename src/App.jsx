2d3279ef285de30b059b6b8979a24aa69acb1b94
aswinss18 <aswinartography18@gmail.com>
2024-10-02 12:43:15 +0530

    Some styled components and GlobalStyle component is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
   return (
     <>
-      <div>Hekll</div>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading type="h1">The Wild Oasis</Heading>
+        <Heading type="h2">Check in and out</Heading>
+        <Button onClick={() => alert("Check in")}>Check in</Button>
+        <Button onClick={() => alert("Check out")}>Check Out</Button>
+        <Heading type="h3">Form</Heading>
+        <Input type="number" placeholder="Number of guests"></Input>
+      </StyledApp>
     </>
   );
 }
