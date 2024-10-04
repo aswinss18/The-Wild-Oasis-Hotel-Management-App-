3ec34bed1e78b09bbbdd386f21021b9210dab6f7
aswinss18 <aswinartography18@gmail.com>
2024-10-04 21:57:12 +0530

    create new cabin functionality is created

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -11,6 +11,19 @@ export async function getCabins() {
   return data;
 }
 
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from("cabin")
+    .insert([newCabin])
+    .select();
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be created");
+  }
+
+  return data;
+}
+
 export async function deleteCabin(id) {
   const { data, error } = await supabase.from("cabin").delete().eq("id", id);
 
