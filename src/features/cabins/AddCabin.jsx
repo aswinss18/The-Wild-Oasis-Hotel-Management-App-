da9b79db4cd32690bea2d36000b0e692476f7e76
aswinss18 <aswinartography18@gmail.com>
2024-10-12 16:57:42 +0530

    Modal component is created to show the craete cabin form

---
+++ src/features/cabins/AddCabin.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import Button from "../../ui/Button";
+import CreateCabinForm from "./CreateCabinForm";
+import Modal from "../../ui/Modal";
+
+export default function AddCabin() {
+  const [isOpenModal, setisOpenModal] = useState(false);
+  return (
+    <div>
+      {" "}
+      <Button onClick={() => setisOpenModal((show) => !show)}>
+        Add new cabin
+      </Button>
+      {isOpenModal && (
+        <Modal onClose={() => setisOpenModal(false)}>
+          <CreateCabinForm onCloseModal={() => setisOpenModal(false)} />
+        </Modal>
+      )}
+    </div>
+  );
+}
